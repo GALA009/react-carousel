8316077e636e2f58d251a3f9b56006de99d2f2bc
张根茂 <zgm4454@gmail.com>
2016-07-07 18:10:02 +0800

    鼠标点击轮换

---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @authors Your Name (you@example.org)
+ * @date    2016-07-04 14:40:28
+ * @version $Id$
+ */
+
+'use strict';
+
+var App = React.createClass({
+
+	render: function() {
+		return (
+			<div className="carouseBox">
+				hello world
+			</div>
+		)；
+	}
+
+});
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
+
