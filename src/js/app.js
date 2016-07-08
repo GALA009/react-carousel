7f5d7b2b0abbb7c65ad623b49721d031528d536a
张根茂 <zgm4454@gmail.com>
2016-07-08 10:30:42 +0800

    git commit -m 'update'

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,16 @@
 /**
  *
- * @authors Your Name (you@example.org)
+ * @authors Zhang Genmao (zgm4454@gmail.com)
  * @date    2016-07-04 14:40:28
- * @version $Id$
+ * @version 0.1
  */
 
 'use strict';
 
 var imgDatas = ['./dep/img/1.png', './dep/img/2.png', './dep/img/3.png'];
+
 var CarouseBox = React.createClass({
-	render: function() {
+	render: function () {
 		return (
 			<div className = "container promo">
 				<div className="row">
@@ -26,50 +27,56 @@ var CarouseItem = React.createClass({
 			pageNum: 0
 		};
 	},
-	handleClickPrev: function() {
+	handleClickPrev: function () {
+		var _self = this.refs.promobg.getDOMNode();
+
 		this.setState(function (state) {
 			state.pageNum = (state.pageNum === 0) ? 2 : state.pageNum - 1;
-			return {pageNum: state.pageNum};
+			return { pageNum: state.pageNum };
 		});
-		if(this.state.pageNum === 0) {
-			this.refs.promobg.getDOMNode().style.cssText="transform: translate3d(-2340px, 0, 0); transition: 500ms ease;";
+
+		if (this.state.pageNum === 0) {
+			_self.style.cssText = "transform: translate3d(-2340px, 0, 0); transition: 500ms ease;";
 		}
 		else if (this.state.pageNum === 1) {
-			this.refs.promobg.getDOMNode().style.cssText="transform: translate3d(0, 0, 0); transition: 500ms ease;";
+			_self.style.cssText = "transform: translate3d(0, 0, 0); transition: 500ms ease;";
 		}
 		else if (this.state.pageNum === 2) {
-			this.refs.promobg.getDOMNode().style.cssText="transform: translate3d(-1170px, 0, 0); transition: 500ms ease;";
+			_self.style.cssText = "transform: translate3d(-1170px, 0, 0); transition: 500ms ease;";
 		}
 	},
-	handleClickNext: function() {
+	handleClickNext: function () {
+		var _self = this.refs.promobg.getDOMNode();
+
 		this.setState(function (state) {
 			state.pageNum = (state.pageNum === 2) ? 0 : state.pageNum + 1;
-			return {pageNum: state.pageNum};
+			return { pageNum: state.pageNum };
 		});
-		if(this.state.pageNum === 0) {
-			this.refs.promobg.getDOMNode().style.cssText="transform: translate3d(-1170px, 0, 0); transition: 500ms ease;";
+
+		if (this.state.pageNum === 0) {
+			_self.style.cssText = "transform: translate3d(-1170px, 0, 0); transition: 500ms ease;";
 		}
 		else if (this.state.pageNum === 1) {
-			this.refs.promobg.getDOMNode().style.cssText="transform: translate3d(-2340px, 0, 0); transition: 500ms ease;";
+			_self.style.cssText = "transform: translate3d(-2340px, 0, 0); transition: 500ms ease;";
 		}
 		else if (this.state.pageNum === 2) {
-			this.refs.promobg.getDOMNode().style.cssText="transform: translate3d(0, 0, 0); transition: 500ms ease;";
+			_self.style.cssText = "transform: translate3d(0, 0, 0); transition: 500ms ease;";
 		}
 	},
-	render: function() {
+	render: function () {
 		var children = [];
 		var nav = [];
 		var pos = 0;
-		var style = {transform: 'translate3d(0, 0, 0)'}
-		for(var i=0; i < imgDatas.length; i++) {
+		var style = { transform: 'translate3d(0, 0, 0)' }
+		for (var i = 0; i < imgDatas.length; i++) {
 			var _style = {
-				left: pos *1170
+				left: pos * 1170
 			}
 			pos++;
 			children.push(
-				< div className = "container-item" style={_style}>
+				<div className = "container-item" style={_style}>
 					<img src={imgDatas[i]} alt='美女' />
-				< /div>);
+				</div>);
 			nav.push(
 				<li className="">
 					<a href="javascript:void(0);"></a>
@@ -77,31 +84,31 @@ var CarouseItem = React.createClass({
 			)
 		}
 		return (
-		<div className="col-lg-12 col-md-12 col-sm-12">
-			<div className="carouse-box" ref="promobg" style={style} key>
-				{children}
-			</div>
-			<div className="promo-ft">
-				<div className="promo-opt col-lg-12 col-md-12 col-sm-12" >
-					<a href="javascript:void(0);" className="prev" onClick={this.handleClickPrev}>
-						<span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-					</a>
-					<a href="javascript:void(0);" className="next" onClick={this.handleClickNext}>
-						<span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-					</a>
+			<div className="col-lg-12 col-md-12 col-sm-12">
+				<div className="carouse-box" ref="promobg" style={style} key='img'>
+					{children}
 				</div>
-				<ul className="promo-nav">
-					{nav}
-				</ul>
-			</div>
+				<div className="promo-ft">
+					<div className="promo-opt col-lg-12 col-md-12 col-sm-12" >
+						<a href="javascript:void(0);" className="prev" onClick={this.handleClickPrev}>
+							<span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						</a>
+						<a href="javascript:void(0);" className="next" onClick={this.handleClickNext}>
+							<span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						</a>
+					</div>
+					<ul className="promo-nav">
+						{nav}
+					</ul>
+				</div>
 
-		</div>
+			</div>
 		);
 	}
 });
 
 
 
-ReactDOM.render( < CarouseBox / > ,
+ReactDOM.render(< CarouseBox / >,
 	document.getElementById('app')
 );
